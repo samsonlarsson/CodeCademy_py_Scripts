b842c2a46f95c0f3277d0aa5bb2618cc3ee3b3c1
Samson Paul <samsonpaul4@gmail.com>
2016-08-19 01:53:39 -0500

    Stocking Out

---
+++ StockingOut.py
@@ -0,0 +1,25 @@
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for item in shopping_list:
+        if stock[item] > 0:
+            total += prices[item]
+        else:
+            stock[item] -= 1
+    print total
