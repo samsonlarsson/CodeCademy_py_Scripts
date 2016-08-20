626b5d4c5ccb9187e6058fcc47472c2f2df7e9fb
Samson Paul <samsonpaul4@gmail.com>
2016-08-20 01:50:21 -0500

    Integer test

---
+++ Practise_Makes_Perfect/is_int.py
@@ -0,0 +1,9 @@
+def is_int(x):
+    
+    if x - (round(x)) > 0:
+        return False
+    else:
+        if x < 0 and x - (round(x)) < 0:
+            return False
+        else:
+            return True
