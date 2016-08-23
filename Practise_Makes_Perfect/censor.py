2e9d7683fd30563e1565c15e129a0139420a08e8
Samson Paul <samsonpaul4@gmail.com>
2016-08-23 00:28:01 -0500

    Censor

---
+++ Practise_Makes_Perfect/censor.py
@@ -0,0 +1,12 @@
+def censor(text, word):
+    words = text.split(" ")
+    w = []
+    for i in words:
+        if i == word:
+            w.append(len(i)*"*")
+        else: 
+            w.append(i)
+    print " ".join(w)
+    return " ".join(w)
+    
+    
