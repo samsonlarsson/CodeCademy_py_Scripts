770d14b77b65e14f3f8516c1e4ab74f7b300feea
Samson Paul <samsonpaul4@gmail.com>
2016-08-28 05:57:28 -0500

    Updates to the class answers

---
+++ Classes/Initializing_a_class.py
@@ -0,0 +1,13 @@
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        
+model = "Delorean"
+color = "silver"
+mpg = 88
+print my_car.condition
+        
+    
